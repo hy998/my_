52147badfb81c7cf5a45db68041ee76f69a1d258
heyu <784343998@qq.com>
2021-05-12 21:22:16 +0800

    'djkieji'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -32,10 +32,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.less$/,
-        loader: "style-loader!css-loader!less-loader",
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
@@ -68,6 +64,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader"
       }
     ]
   },
